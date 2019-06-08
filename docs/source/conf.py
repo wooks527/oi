367117c6f98202379594f6262243147d93ca3e57
Hyunwook Kim <hyunwook.kim.89@gmail.com>
2019-06-08 12:06:48 +0900

    Update custom css and js file location.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -197,5 +197,5 @@ todo_include_todos = True
 
 # Add custom css and js
 def setup(app):
-    app.add_stylesheet('https://github.com/hwkim89/oi/blob/master/docs/build/html/_static/custom.css')
-    app.add_javascript('https://github.com/hwkim89/oi/blob/master/docs/build/html/_static/custom.js')
+    app.add_stylesheet('https://raw.githubusercontent.com/hwkim89/oi/master/docs/build/html/_static/custom.css')
+    app.add_javascript('https://raw.githubusercontent.com/hwkim89/oi/master/docs/build/html/_static/custom.js')
